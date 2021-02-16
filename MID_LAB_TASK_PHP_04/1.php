f019010f3d68b67d4289bb9686ec3712883cb9d1
ShaikhShakibRahman <73012177+ShaikhShakibRahman@users.noreply.github.com>
2021-02-17 01:51:43 +0600

    Mid_Lab_Task_04

---
+++ MID_LAB_TASK_PHP_04/1.php
@@ -0,0 +1,6 @@
+<?php  
+ $length = 15;  
+ $width = 12;  
+ echo "area of rectangle is $length * $width= " . ($length * $width) . "<br />"; 
+ echo "perimeter". 2 * ($length + $width) ;
+  ?>
---
+++ MID_LAB_TASK_PHP_04/4.php
@@ -0,0 +1,15 @@
+<?php
+      $num1=25;
+      $num2=35;
+      $num3=32;
+      if($num1>$num2 && $num1>$num3){
+        echo $num1;
+      }
+      else{
+        if($num2>$num1 && $num2>$num3){
+          echo $num2;
+        }
+        else
+          echo $num3;
+      }
+?>
