7531c2874eee365fec4e1f8149466d2270d8e27a
ShaikhShakibRahman <73012177+ShaikhShakibRahman@users.noreply.github.com>
2021-03-24 11:24:20 +0600

    create

---
+++ FINAL_LAB_TASK_01_DB/view/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Landing pange</title>
+</head>
+<body>
+
+	<center>
+		<h1>Welcome</h1>
+		<a href="login.php"> Login</a> | 
+		<a href="reg.php"> Signup</a>  
+	</center>
+</body>
+</html>
